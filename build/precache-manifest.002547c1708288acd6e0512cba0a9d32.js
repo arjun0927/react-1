0d205e947a8993dcab1e4297c1b752bd463a8f51
Arjun Singh <as7906929883@gmail.com>
2023-08-06 16:30:03 +0530

    initial

---
+++ build/precache-manifest.002547c1708288acd6e0512cba0a9d32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb13968c5429ce5ba6d4037a18f48693",
+    "url": "/miStore-clone/index.html"
+  },
+  {
+    "revision": "205ff043d2dc990967a4",
+    "url": "/miStore-clone/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "bfbbb4185804de7999cc",
+    "url": "/miStore-clone/static/css/main.ddfa7f1d.chunk.css"
+  },
+  {
+    "revision": "205ff043d2dc990967a4",
+    "url": "/miStore-clone/static/js/2.55bc69a3.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/miStore-clone/static/js/2.55bc69a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbbb4185804de7999cc",
+    "url": "/miStore-clone/static/js/main.d5f0f7c4.chunk.js"
+  },
+  {
+    "revision": "1850f956b0ad06fe52d2",
+    "url": "/miStore-clone/static/js/runtime-main.ded4a024.js"
+  }
+]);
